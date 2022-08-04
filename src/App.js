5709c5234459c5ffff0dd56638658898b87ea759
SvitlanaBurlaka <burlaka.sv.@gmail.com>
2022-08-04 17:08:32 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { AboutMe } from "./components/AboutMe/AboutMe";
 import { MySkills } from "../src/components/MySkills/MySkills";
 import { MyProjects } from "../src/components/MyProjects/MyProjects";
 import { ContactMe } from "../src/components/ContactMe/ContactMe";
-// import { LandingPage } from "./components/LandingPage";
 
 function App() {
   return (
@@ -16,18 +15,6 @@ function App() {
         <MySkills />
         <MyProjects />
         <ContactMe />
-        {/* <Route exact path="/">
-          <AboutMe />
-        </Route>
-        <Route path="/mySkills">
-          <MySkills />
-        </Route>
-        <Route path="/projects">
-          <MyProjects />
-        </Route>
-        <Route path="/contact">
-          <ContactMe />
-        </Route> */}
       </div>
     </BrowserRouter>
   );
